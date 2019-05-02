75a39610087d8bb1f1f95761b60b472f1f94dfc1
apanichella <a.panichella@tudelft.nl>
2019-05-02 12:12:49 +0200

    Reducing the complexity of the Euclidean distance

--- client/src/main/java/org/evosuite/ga/operators/ranking/EuclideanDistance.java
+++ client/src/main/java/org/evosuite/ga/operators/ranking/EuclideanDistance.java
@@ -14,26 +14,21 @@ public class EuclideanDistance<T extends Chromosome> extends SecondaryRanking<T>
 
     private static final long serialVersionUID = 7955426596562779202L;
 
-    private static final Logger logger = LoggerFactory.getLogger(CoverageDiversity.class);
+    private static final Logger logger = LoggerFactory.getLogger(EuclideanDistance.class);
 
     @Override
     public void assignSecondaryRank(List<T> front, Set<FitnessFunction<T>> set){
-        double distance;
+        double[][] distances = new double[front.size()][front.size()];
 
-        for (T t1 : front){
-            TestChromosome tch1 =  (TestChromosome) t1;
+        for (int i=0; i<front.size()-1; i++){
+            TestChromosome tch1 =  (TestChromosome) front.get(i);
             ExecutionResult result1 = tch1.getLastExecutionResult();
             Map<Integer, Integer> frequency1 = result1.getTrace().getNoExecutionForConditionalNode();
-            double difference = Double.MAX_VALUE;
-            for(T t2 : front){
-                if (t1 == t2)
-                    continue;
 
-                TestChromosome tch2 =  (TestChromosome) t2;
+            for (int j=i+1; j<front.size(); j++){
+                TestChromosome tch2 =  (TestChromosome) front.get(j);
                 ExecutionResult result2 = tch2.getLastExecutionResult();
                 Map<Integer, Integer> frequency2 = result2.getTrace().getNoExecutionForConditionalNode();
-
-                distance = 0;
 
                 HashSet<Integer> union = new HashSet<>();
                 union.addAll(frequency1.keySet());
@@ -41,20 +36,27 @@ public class EuclideanDistance<T extends Chromosome> extends SecondaryRanking<T>
 
                 for (Integer branch_id : union){
                     if (!frequency1.containsKey(branch_id))
-                        distance += Math.pow(frequency2.get(branch_id), 2);
+                        distances[i][j] += Math.pow(frequency2.get(branch_id), 2);
                     else if (!frequency2.containsKey(branch_id))
-                        distance += Math.pow(frequency1.get(branch_id), 2);
+                        distances[i][j] += Math.pow(frequency1.get(branch_id), 2);
                     else
-                        distance += Math.pow(frequency1.get(branch_id)-frequency2.get(branch_id), 2);
+                        distances[i][j] += Math.pow(frequency1.get(branch_id)-frequency2.get(branch_id), 2);
                 }
-
-                difference = Math.min(difference, distance);
-                difference = difference / set.size();
-
+                distances[j][i] = distances[i][j];
             }
-            tch1.setDistance(difference);
-        }
 
+        }
+        for (int i=0; i<front.size(); i++){
+            T t = front.get(i);
+            double min = Double.MAX_VALUE;
+            for (int j=0; j<front.size(); j++){
+                if (i==j)
+                    continue;
+
+                min = Math.min(distances[i][j], min);
+            }
+            t.setDistance(min/(min+1));
+        }
         //for (T test : front){
         //    logger.error("Test {} with distance {}", test.hashCode(), test.getDistance());
         //}
